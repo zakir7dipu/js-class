8b51153af40af39fdfb8f358a12c5380ac052c4c
zakir7dipu <zakir67@dipti.com.bd>
2021-11-03 23:04:03 +0600

    update

--- js class/SynchronousAndAsynchronous.js
+++ js class/SynchronousAndAsynchronous.js
@@ -1,7 +1,7 @@
 // Synchronous ************
 // Asynchronous ***********
 //
-console.clear();
+// console.clear();
 const show = () => {
     return new Promise((resolve, reject) => {
         resolve('This a from show 0 function')
@@ -55,36 +55,66 @@ const show6 = () => {
 // show6();
 
 // Promise **********
-show()
-    .then(res => {
-        console.log(res)
-    })
-    .then(show1)
-    .then(res => {
-        console.log(res)
-    })
-    .then(show2)
-    .then(res => {
-        console.log(res)
-    })
-    .then(show3)
-    .then(res => {
-        console.log(res)
-    })
-    .then(show4)
-    .then(res => {
-        console.log(res)
-    })
-    .then(show5)
-    .then(res => {
-        console.log(res)
-    })
-    .then(show6)
-    .then(res => {
-        console.log(res)
-    })
-    .catch(err =>{
-        console.log(err)
-    });
+// show()
+//     .then(res => {
+//         console.log(res)
+//     })
+//     .then(show1)
+//     .then(res => {
+//         console.log(res)
+//     })
+//     .then(show2)
+//     .then(res => {
+//         console.log(res)
+//     })
+//     .then(show3)
+//     .then(res => {
+//         console.log(res)
+//     })
+//     .then(show4)
+//     .then(res => {
+//         console.log(res)
+//     })
+//     .then(show5)
+//     .then(res => {
+//         console.log(res)
+//     })
+//     .then(show6)
+//     .then(res => {
+//         console.log(res)
+//     })
+//     .catch(err =>{
+//         console.log(err)
+//     });
 
+// async await
+
+// async function showAllProAtTrF(){
+//     let s0 = await show();
+//     console.log(s0);
+// }
+// showAllProAtTrF();
+
+const showAllProAtArF = async () => {
+   try {
+       let s0 = await show();
+       let s1 = await show1();
+       let s2 = await show2();
+       let s3 = await show3();
+       let s4 = await show4();
+       let s5 = await show5();
+       let s6 = await show6();
+
+       console.log(s0);
+       console.log(s1);
+       console.log(s2);
+       console.log(s3);
+       console.log(s4);
+       console.log(s5);
+       console.log(s6);
+   }catch (err){
+       console.log(err)
+   }
+}
+showAllProAtArF();
 // [XMLHttpRequest, fetch request, axios, jquery ajax]
---
+++ js class/xml-http-request.js
@@ -0,0 +1,67 @@
+console.clear();
+
+// let container = document.querySelector('tbody');
+// Array.from(data).map(item => {
+//     let row = document.createElement('tr');
+//     let titleColumn = document.createElement('td');
+//     titleColumn.innerText = item.title;
+//     let bodyColumn = document.createElement('td');
+//     bodyColumn.innerText = item.body;
+//     row.appendChild(titleColumn);
+//     row.appendChild(bodyColumn);
+//     container.appendChild(row);
+// })
+
+
+
+const makeRequest = (method, url, data) => {
+    let xml = new XMLHttpRequest();
+    xml.open(method,url);
+    xml.setRequestHeader('Content-type', 'application/json');
+    xml.onload = () => {
+        let data = xml.response;
+        console.log(JSON.parse(data));
+    }
+
+    xml.onerror = () => {
+        console.log('We don\'t get any data')
+    }
+
+    xml.send(JSON.stringify(data));
+}
+
+const getdata = () => {
+    makeRequest('get', 'https://jsonplaceholder.typicode.com/posts/1');
+}
+// getdata();
+
+const postdata = () => {
+    makeRequest('post', 'https://jsonplaceholder.typicode.com/posts',{
+        title: 'foo215456',
+        body: 'barasdadqqwe',
+        userId: 1,
+    });
+}
+// postdata();
+
+const updatedata = () => {
+    makeRequest('PUT', 'https://jsonplaceholder.typicode.com/posts/2',{
+        id: '2',
+        title: 'foo215456',
+        body: 'barasdadqqwe',
+        userId: 1,
+    });
+}
+// updatedata();
+
+const updatesingeldata = () => {
+    makeRequest('PATCH', 'https://jsonplaceholder.typicode.com/posts/2',{
+        title: 'foo215456895',
+    });
+}
+// updatesingeldata();
+
+const deletedata = () => {
+    makeRequest('DELETE', 'https://jsonplaceholder.typicode.com/posts/2');
+}
+// deletedata();
