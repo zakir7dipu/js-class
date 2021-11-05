5508bc36f894ff4840897b0b0e4b4041dc3c8862
zakir7dipu <zakir67@dipti.com.bd>
2021-11-05 18:38:45 +0600

    update

--- js class/pagination.js
+++ js class/pagination.js
@@ -1,13 +1,15 @@
+// step 1
 let listingTable = document.querySelector('tbody');
 let btnNext = document.getElementById("btn_next");
 let btnPrev = document.getElementById("btn_prev");
 let pageSpan = document.getElementById("page");
 
-
+// step 2
 let currentPage = 1;
 let recordsPerPage = 5;
 let allPosts = [];
 
+// step 3
 const makeRequest = async (url, config) => {
     let res = await fetch(url, config)
     if (!res.ok){
@@ -16,6 +18,7 @@ const makeRequest = async (url, config) => {
     return res.json();
 }
 
+// step 4
 const getAllData = () => {
     makeRequest('https://jsonplaceholder.typicode.com/posts')
         .then(res => {
@@ -25,6 +28,7 @@ const getAllData = () => {
 }
 getAllData();
 
+// step 5
 const prevPage = () => {
     if (currentPage > 1) {
         currentPage--;
@@ -33,7 +37,7 @@ const prevPage = () => {
 }
 btnPrev.addEventListener('click', prevPage)
 
-
+// step 6
 const nextPage = () => {
     if (currentPage < numPages()) {
         currentPage++;
@@ -42,10 +46,12 @@ const nextPage = () => {
 }
 btnNext.addEventListener('click', nextPage)
 
+// step 7
 const numPages = () => {
     return Math.ceil(allPosts.length / recordsPerPage);
 }
 
+// step 8
 const changePage = page => {
     // Validate page
     if (page < 1) page = 1;
@@ -71,6 +77,7 @@ const changePage = page => {
     }
 }
 
+// step 9
 const show = (res) => {
     Array.from(res).map(item => {
         let row = document.createElement('tr');
