4fd34d34e338f397b93004468152d3778ab4436e
Zakir Hossain <zakir7dipu@gmail.com>
2021-11-29 22:11:22 +0600

    update

--- OurFirstProject/server.js
+++ OurFirstProject/server.js
@@ -6,7 +6,27 @@ const app = express();
 app.use('/assets', express.static(path.resolve(__dirname,'assets')));
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname+ '/forntend/index.html'))
+    // res.sendFile(path.resolve(__dirname+ '/forntend/index.html'))
+    let data = {
+        info: [
+            {"id": "1", "name": "Kamal", "age": "35"},//<- element = object, string, int, bool, array
+            {id: 2, name: 'Korim', age: 30},//<- ok
+            {id: 3, name: 'Josim', age: 23},//<- ok
+            {id: 4, name: 'Mofij', age: 25},//<-
+            {id: 5, name: 'Sokiq', age: 45},//<-
+            {id: 6, name: 'Kamrul', age: 65},//<-
+        ]
+    }
+    let data2 = {
+        name: 'Bag \b',
+        type: [
+            {name: 'M-Bag'},
+            {name: 'M-Bag'}
+        ],
+        price:{dis: 120, reg: 1500},
+        "userName": 'xyz'
+    }
+    res.send(JSON.stringify(data2))
 });
 
 app.get('/about',(req,res)=>{
@@ -14,4 +34,3 @@ app.get('/about',(req,res)=>{
 });
 
 app.listen(process.env.PORT || 3000, () => console.log('server is running'));
-
