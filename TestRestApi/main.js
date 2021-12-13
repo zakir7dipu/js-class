7e463b94517a315fa513a79c4ba36296c04e29d6
zakir7dipu <zakir67@dipti.com.bd>
2021-12-13 22:36:05 +0600

    update

--- TestRestApi/main.js
+++ TestRestApi/main.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+// body parser
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
 app.use('/api/student', require('./routs/student'));
 
 const port = process.env.PORT || 3000;
--- TestRestApi/routs/student.js
+++ TestRestApi/routs/student.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const uuid = require('uuid');
 const fs = require('fs');
 
 // get all student
@@ -29,6 +30,38 @@ router.get('/:id', (req, res) => {
     }else {
         res.status(400).json({message:'No data found.'})
     }
+});
+
+// post a student
+router.post('/', (req, res) => {
+    // make an object
+    let newStudent = {
+        "id": uuid.v4(),
+        "name": req.body.name,
+        "roll": req.body.roll,
+        "phone": req.body.phone
+    }
+
+    // validate new data
+    if (!newStudent.name || !newStudent.roll || !newStudent.phone){
+        res.status(400).json({message:'Please insert data properly...'})
+    }
+
+    // store new data
+    let data = fs.readFileSync('./student.json', 'utf-8');
+    data = JSON.parse(data);
+    data = data.students;
+    data.push(newStudent);
+    let newData = {
+        students: data
+    };
+    fs.writeFile('./student.json', JSON.stringify(newData,null, 2), (err) => {
+        if (err) {
+            throw err
+        } else {
+            res.json({message: "Student saved successfully.", newStudent})
+        }
+    })
 })
 
 module.exports = router;
