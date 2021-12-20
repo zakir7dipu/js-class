ab52eeae4d7f408dd3dd87141c713d369051463b
zakir7dipu <zakir67@dipti.com.bd>
2021-12-20 22:51:55 +0600

    update

--- TestRestApi/routs/student.js
+++ TestRestApi/routs/student.js
@@ -62,6 +62,66 @@ router.post('/', (req, res) => {
             res.json({message: "Student saved successfully.", newStudent})
         }
     })
+});
+
+// edit a student
+router.put('/:id', (req, res) => {
+    let data = fs.readFileSync('./student.json', 'utf-8');
+    data = JSON.parse(data);
+    data = data.students;
+    if (data.length > 0){
+        const found = data.filter(item => parseInt(item.id) === parseInt(req.params.id))
+        if (found.length > 0){
+            // make an object
+            let editStudent = found[0];
+            editStudent.name = !req.body.name?editStudent.name:req.body.name;
+            editStudent.roll = !req.body.roll?editStudent.roll:req.body.roll;
+            editStudent.phone = !req.body.phone?editStudent.phone:req.body.phone;
+
+            // store edit data
+            let newData = {
+                students: data
+            };
+            fs.writeFile('./student.json', JSON.stringify(newData,null, 2), (err) => {
+                if (err) {
+                    throw err
+                } else {
+                    res.json({message: "Student updated successfully.", editStudent})
+                }
+            })
+        }else {
+            res.status(400).json({message:`No data found against ${req.params.id}`})
+        }
+    }else {
+        res.status(400).json({message:'No data found.'})
+    }
+});
+
+// delete a student
+router.delete('/:id', (req , res) => {
+    let data = fs.readFileSync('./student.json', 'utf-8');
+    data = JSON.parse(data);
+    data = data.students;
+    if (data.length > 0){
+        Array.from(data).map((item, index) => {
+            if (parseInt(req.params.id) === parseInt(item.id)){
+                data.splice(index,1)
+            }
+        })
+        // store edit data
+        let newData = {
+            students: data
+        };
+        fs.writeFile('./student.json', JSON.stringify(newData,null, 2), (err) => {
+            if (err) {
+                throw err
+            } else {
+                res.json({message: "Student deleted successfully."})
+            }
+        })
+    }else {
+        res.status(400).json({message:'No data found.'})
+    }
 })
 
 module.exports = router;
